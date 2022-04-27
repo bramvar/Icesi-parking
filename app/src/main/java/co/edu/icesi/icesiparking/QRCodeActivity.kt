a7fa17bf9bdbaa5441527a3cff710ca5a12cecac
J. Var <bramf0@protonmail.com>
2022-04-27 16:55:20 -0500

    add principal activities

---
+++ app/src/main/java/co/edu/icesi/icesiparking/QRCodeActivity.kt
@@ -0,0 +1,11 @@
+package co.edu.icesi.icesiparking
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class QRCodeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_qrcode)
+    }
+}
