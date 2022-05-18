79269021fd639db2eee39116471044212d90a765
J. Var <bramf0@protonmail.com>
2022-05-17 23:33:36 -0500

    add navigation to user settings from user home

--- app/src/main/java/co/edu/icesi/icesiparking/UserHomeActivity.kt
+++ app/src/main/java/co/edu/icesi/icesiparking/UserHomeActivity.kt
@@ -1,11 +1,22 @@
 package co.edu.icesi.icesiparking
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import co.edu.icesi.icesiparking.databinding.ActivityUserHomeBinding
 
 class UserHomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityUserHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_user_home)
+
+        binding = ActivityUserHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.userSettingsButton.setOnClickListener {
+            startActivity(Intent(this,SettingsActivity::class.java))
+        }
     }
 }
