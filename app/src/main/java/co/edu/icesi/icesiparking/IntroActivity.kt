c80adfb62b915c2630806712889e33dd2527d108
J. Var <bramf0@protonmail.com>
2022-05-17 19:02:49 -0500

    add connection to user home

--- app/src/main/java/co/edu/icesi/icesiparking/IntroActivity.kt
+++ app/src/main/java/co/edu/icesi/icesiparking/IntroActivity.kt
@@ -1,11 +1,22 @@
 package co.edu.icesi.icesiparking
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import co.edu.icesi.icesiparking.databinding.ActivityIntroBinding
 
 class IntroActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityIntroBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_intro)
+
+        binding = ActivityIntroBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.introStartBtn.setOnClickListener {
+            startActivity(Intent(this,UserHomeActivity::class.java))
+        }
     }
 }
