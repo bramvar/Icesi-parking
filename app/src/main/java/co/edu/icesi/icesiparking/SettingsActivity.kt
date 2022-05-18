691804486e9bf2a69a8865cef0f38323ade043f4
J. Var <bramf0@protonmail.com>
2022-05-17 23:39:35 -0500

    implement gallery launcher to pick an image

--- app/src/main/java/co/edu/icesi/icesiparking/SettingsActivity.kt
+++ app/src/main/java/co/edu/icesi/icesiparking/SettingsActivity.kt
@@ -1,11 +1,38 @@
 package co.edu.icesi.icesiparking
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import co.edu.icesi.icesiparking.databinding.ActivitySettingsBinding
+import co.edu.icesi.icesiparking.util.UtilDomi
 
 class SettingsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySettingsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_settings)
+
+        binding = ActivitySettingsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val galleryLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult(),::onGalleryResult)
+
+        binding.updateUserImage.setOnClickListener{
+            val intent = Intent(Intent.ACTION_GET_CONTENT)
+            intent.type = "image/*"
+            galleryLauncher.launch(intent)
+        }
+    }
+
+    private fun onGalleryResult(activityResult: ActivityResult){
+        if(activityResult.resultCode == RESULT_OK){
+            val uri = activityResult.data?.data
+            binding.updateUserImage.setImageURI(uri)
+        }
+
     }
 }
