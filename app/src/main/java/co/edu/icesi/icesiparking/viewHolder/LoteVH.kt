115ddfd7795fdf3a4cefddff5509b3c2938e5fd7
J. Var <bramf0@protonmail.com>
2022-05-18 19:14:25 -0500

    implement functionality of loading data in lote screen. Add navigation to lote screen

--- app/src/main/java/co/edu/icesi/icesiparking/viewHolder/LoteVH.kt
+++ app/src/main/java/co/edu/icesi/icesiparking/viewHolder/LoteVH.kt
@@ -1,14 +1,18 @@
 package co.edu.icesi.icesiparking.viewHolder
 
+import android.content.Intent
 import android.view.View
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import co.edu.icesi.icesiparking.LoteActivity
 import co.edu.icesi.icesiparking.R
 import co.edu.icesi.icesiparking.model.Lote
 import com.bumptech.glide.Glide
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.ktx.storage
+import java.io.Serializable
 
 class LoteVH(itemView:View): RecyclerView.ViewHolder(itemView) {
 
@@ -20,6 +24,13 @@ class LoteVH(itemView:View): RecyclerView.ViewHolder(itemView) {
     val loteShortDescription: TextView = itemView.findViewById(R.id.shortDescriptionTextView)
     val loteState: TextView = itemView.findViewById(R.id.loteStateTextView)
     val loteImage: ImageView = itemView.findViewById(R.id.loteImage)
+    val openLoteButton: Button = itemView.findViewById(R.id.openLoteButton)
+
+    init {
+        openLoteButton.setOnClickListener {
+            openLoteView()
+        }
+    }
 
     fun bindLote(lote:Lote){
         this.lote = lote
@@ -39,6 +50,16 @@ class LoteVH(itemView:View): RecyclerView.ViewHolder(itemView) {
                     Glide.with(loteImage).load(it).into(loteImage)
                 }
         }
+
+    }
+
+    fun openLoteView(){
+        val lote = this.lote
+
+        val intent = Intent(openLoteButton.context, LoteActivity::class.java).apply {
+            putExtra("lote",lote as Serializable)
+        }
+        openLoteButton.context.startActivity(intent)
 
     }
 }
